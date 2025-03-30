755bdda1e40ee92903ded236a1360ebef10d8a3b
JokAlex <JokAlex2005@yandex.ru>
2025-03-30 11:56:53 +0300

    42 added

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import test
 import test2
+import module42
 
 print("hello, world")
 print(test.test())
 print(test.test2())
 print(test2.test2())
+print(module42.answer())
 print("goodbye, world!")
